4925eb0ddfafadc79109f674feee05812297cce7
Taerrean <terrar1umand@gmail.com>
2026-05-07 22:34:39 +0300

    Add files via upload

--- 9756/9765code.py
+++ 9756/9765code.py
@@ -2,10 +2,9 @@ def arrange_meetings(mass):
     t = 0
     every_end = []
     while mass:
-        every_end.append(mass[0]) if not all(x < min(mass, key= lambda x: x[1])[1] for x, y in mass) else every_end.append(max(mass, key= lambda x: x[1]))
+        every_end.append(mass[0]) if not max(mass)[0] < min(mass, key= lambda x: x[1])[1] else every_end.append(max(mass, key= lambda x: x[1]))
         t = every_end[-1][1]
-        a = [meet for meet in mass if meet[0] >= t and meet not in every_end]
-        mass = a
+        mass = [meet for meet in mass if meet[0] >= t and meet not in every_end]
     return [len(every_end), t]
 f = open('26_9756.txt')
 n = int(f.readline())
@@ -13,5 +12,5 @@ data = []
 for line in f:
     n, k = [int(k) for k in line.split()]
     data.append([n, k])
-data.sort(key =lambda x: x[1])
+data.sort(key= lambda x: x[1])
 print(arrange_meetings(data))
